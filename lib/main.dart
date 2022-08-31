6cbc8104167d7816eb19a62b1a3080c70ffdf79f
ahmadshofi23 <ahmadshofihasibuan12@gmail.com>
2022-09-01 01:46:17 +0700

    implement modularization

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:testmobileptsinarmas/feature/presentation/page/detail.page.dart';
 import 'package:testmobileptsinarmas/feature/presentation/page/home.page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ModularApp(module: AppModule(), child: const MyApp()));
+}
+
+class AppModule extends Module {
+  @override
+  List<Bind> get binds => [];
+
+  @override
+  List<ModularRoute> get routes => [
+        ChildRoute('/', child: (context, args) => const HomePage()),
+        ChildRoute('/detail',
+            child: (context, args) => const DetailFood(),
+            transition: TransitionType.rightToLeft),
+      ];
 }
 
 class MyApp extends StatelessWidget {
@@ -10,13 +25,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
+      routeInformationParser: Modular.routeInformationParser,
+      routerDelegate: Modular.routerDelegate,
     );
   }
 }
